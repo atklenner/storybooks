926f827a4c7bcddd6f263b7bc0b70782905da5ed
Andrew Klenner <atklenner@gmail.com>
2022-09-21 19:56:44 -0700

    create logout route

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,11 @@ router.get(
   (req, res) => res.redirect("/dashboard")
 );
 
+// Logout user
+// GET /auth/logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
