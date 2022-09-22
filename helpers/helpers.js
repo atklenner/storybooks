d4b7277aa1af21f6ef9b149a60e6a9720ba99001
Andrew Klenner <atklenner@gmail.com>
2022-09-22 13:02:03 -0700

    add editIcon helper

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,6 +1,7 @@
-const { truncate, stripTags } = require("./ejs");
+const { truncate, stripTags, editIcon } = require("./ejs");
 
 module.exports = (app) => {
   app.locals.truncate = truncate;
   app.locals.stripTags = stripTags;
+  app.locals.editIcon = editIcon;
 };
